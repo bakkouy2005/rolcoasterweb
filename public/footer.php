95e9cefa6e2f76d850a34ea6041f88807b01fb4e
bakkouy2005 <154227141+bakkouy2005@users.noreply.github.com>
2024-03-14 14:51:46 +0100

    done

---
+++ public/footer.php
@@ -0,0 +1,12 @@
+<header>
+        <div class="wrapper">
+        <div class="logo"><img src="img/css/logo.jpg" alt=""></div>
+        <nav>
+            <a href="index.php"><i class="fa-solid fa-house icon" style="color: white"></i>home</a>
+            <a href="gebet.php"><i class="fa-solid fa-solid fa-clock" style="color: white;"></i>openingstijden</a>
+            <a href="spenden.php"><i class="fa-solid fa-hand-holding-dollar" style="color: white;"></i>prijzen</a>
+            <a href="info.php"><i class="fa-solid fa-circle-info" style="color: white"></i>info</a>
+            
+        </nav>
+        </div>
+</header>
---
+++ public/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include "head.php" ?>
+    <title>website</title>
+    <link rel="stylesheet" href="img/css/style.css">
+</head>
+<body>
+    <div class="hero" id="photo">
+    <?php include "header.php" ?>
+        <div class="hero-text">
+            <div class="mini-logo"><img src="img/css/piton.jpeg" alt="">
+                <h1 style="font-size:50px"></h1>
+                <p></p>
+                <button id="btspenden">67.000</button>
+            </div>
+
+            <div class="mini-logo"><img src="img/css/baron.jpeg" alt="">
+                <h1 style="font-size:50px"></h1>
+                <p></p>
+                <button id="btspenden">30.000 euro</button>
+            </div>
+
+            <div class="mini-logo"><img src="img/css/draak.jpeg" alt="">
+                <h1 style="font-size:50px"></h1>
+                <p></p>
+                <button id="btspenden">100.000</button>
+            </div>
+            <div class="mini-logo"><img src="img/css/holland.jpg" alt="">
+                <h1 style="font-size:50px"></h1>
+                <p></p>
+                <button id="btspenden">340.000</button>
+            </div>
+        </div>
+    </div>
+        <div class="elfsight-app-a9277d97-60ab-410b-b0d5-ecdb9d796928" data-elfsight-app-lazy></div>
+        <div class="footerbackground">
+        <footer>
+            <div class="wrapper">
+                
+            </div>
+        </footer>
+        </div>
+</body>
+</html>
